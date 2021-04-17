8812c5035dd90069eb847cea0e6e0d9530869ece
bikucrypt <bijukrishnan1992@gmail.com>
2021-04-17 23:06:48 +0000

    added listings app and models

---
+++ listings/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+class Listing(models.Model):
+
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    listing_name = models.CharField(max_length=250, null=True, blank=True,)
+    price = models.CharField(max_length=250, null=True, blank=True,)
+    short_description = models.CharField(max_length=720)
+    description = models.TextField()
+    bedroom = models.CharField(max_length=250, null=True, blank=True,)
+    bathroom = models.CharField(max_length=250, null=True, blank=True,)
+    lease = models.CharField(max_length=250)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.listing_name
