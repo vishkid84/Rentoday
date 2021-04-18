e700fcbc064a97d20b6ff8889dcfc8b28c895166
bikucrypt <bijukrishnan1992@gmail.com>
2021-04-18 22:11:17 +0000

    added styling and content to listings.html

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+
+from .models import Listing
 
 # Create your views here.
+
+
+def listings(request):
+    listings = Listing.objects.all().order_by('-date')
+
+    template = 'listings/listings.html'
+    context = {
+        'listings': listings,
+    }
+
+    return render(request, template, context)
