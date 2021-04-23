8f7c295bdd1895418510aca33ed1cb58015e5af1
bikucrypt <bijukrishnan1992@gmail.com>
2021-04-23 23:43:17 +0000

    added latest listings to home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from listings.models import Listing
+from listings.views import listings
+
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'home/index.html')
+    listings = Listing.objects.order_by('-date')[0:3]
+
+    context = {
+        'listings': listings,
+    }
+    return render(request, 'home/index.html', context)
